2ca162a9d071f5df72974f7f665754b42eb2aa77
Marocz11 <marek.halska@gmail.com>
2023-05-09 14:15:25 +0200

    with summary sheet

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import time
 import openpyxl
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from openpyxl.utils import column_index_from_string
+import requests
+import json
+import requests
+from bs4 import BeautifulSoup
+from yahoofinancials import YahooFinancials
+from forex_python.converter import CurrencyRates
 
-def scrape_product(driver, product_id, url_prefix):
+
+def get_exchange_rate(base_currency, target_currency):
+    cr = CurrencyRates()
+    exchange_rate = cr.get_rate(base_currency, target_currency)
+    return exchange_rate
+
+def scrape_product(driver, product_id, url_prefix, currency):
     url = f"{url_prefix}{product_id}"
     driver.get(url)
     time.sleep(5)
@@ -18,16 +32,25 @@ def scrape_product(driver, product_id, url_prefix):
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "pip-header-section")))
     product_name = driver.find_element_by_css_selector(".pip-header-section__title--big").text
     product_price = driver.find_element_by_css_selector(".pip-temp-price__integer").text
+    product_price_str = re.sub(r'[^\d]', '', product_price)
+    
     product_code = driver.find_element_by_css_selector(".pip-product-identifier__value").text
     product_description = driver.find_element_by_css_selector(".pip-header-section__description-text").text
     product_measurement = driver.find_element_by_css_selector(".pip-header-section__description-measurement").text
 
+    if currency == "PLN":
+        product_price_pln = int(product_price_str)
+        product_price_czk = int(product_price_str) * get_exchange_rate("PLN", "CZK")
+    elif currency == "CZK":
+        product_price_czk = int(product_price_str)
+        product_price_pln = int(product_price_str) * get_exchange_rate("CZK", "PLN")
+    
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
 
-    return [product_name, product_price, product_code, product_description, product_measurement]
+    return [product_name, product_price_pln, product_price_czk, product_code, product_description, product_measurement]
 
-def write_to_excel(workbook, data, sheet_name):
+def write_to_excel(workbook, data, sheet_name, currency):
     if not data:
         return
 
@@ -37,13 +60,53 @@ def write_to_excel(workbook, data, sheet_name):
     else:
         sheet = workbook.create_sheet(sheet_name)
 
-    headers = ["Product Name", "Product Price", "Product Code", "Product Description", "Product Measurement"]
+    headers = ["Product Name", "Product Price (PLN)", "Product Price (CZK)",
+    "Product Code", "Product Description", "Product Measurement"]
     for col_num, header in enumerate(headers, 1):
         sheet.cell(row=1, column=col_num, value=header)
 
     for row_num, row_data in enumerate(data, 2):
         for col_num, cell_data in enumerate(row_data, 1):
             sheet.cell(row=row_num, column=col_num, value=cell_data)
+    
+
+    sum_price_pln_col = column_index_from_string("B")
+    sum_price_czk_col = column_index_from_string("C")
+    last_row = sheet.max_row
+    sheet.cell(row=last_row + 1, column=sum_price_pln_col, value=f"=SUM(B2:B{last_row})")
+    sheet.cell(row=last_row + 1, column=sum_price_czk_col, value=f"=SUM(C2:C{last_row})")
+
+def create_summary_sheet(workbook, data_cz, data_pl):
+    summary_sheet = workbook.active
+    summary_sheet.title = "Summary"
+    
+    headers = ["Product Name", "Product Code", "Product Measurement", "Czech Price", "Poland Price"]
+    for col_num, header in enumerate(headers, 1):
+        summary_sheet.cell(row=1, column=col_num, value=header)
+
+    for index, (cz_row, pl_row) in enumerate(zip(data_cz, data_pl), 2):
+        product_name_cz, price_pln_cz, price_czk_cz, product_code_cz, product_description_cz, product_measurement_cz = cz_row
+        product_name_pl, price_pln_pl, price_czk_pl, product_code_pl, product_description_pl, product_measurement_pl = pl_row
+
+        if product_name_cz == product_name_pl:
+            product_name = product_name_cz
+        else:
+            product_name = f"{product_name_cz} / {product_name_pl}"
+
+        summary_data = [product_name, product_code_cz, product_measurement_cz, price_czk_cz, price_czk_pl]
+        for col_num, cell_data in enumerate(summary_data, 1):
+            summary_sheet.cell(row=index, column=col_num, value=cell_data)
+
+    last_row = summary_sheet.max_row
+
+    summary_sheet.cell(row=last_row + 1, column=1, value="Total:")
+    summary_sheet.cell(row=last_row + 1, column=4, value=f"=SUM(D2:D{last_row})")
+    summary_sheet.cell(row=last_row + 1, column=5, value=f"=SUM(E2:E{last_row})")
+
+    # Add formatting for the total row
+    for col_num in range(1, 6):
+        summary_sheet.cell(row=last_row + 1, column=col_num).font = openpyxl.styles.Font(bold=True)
+
 
 def main():
     product_ids = "394.374.11, 395.006.24, 694.780.23"
@@ -54,19 +117,23 @@ def main():
 
     data_cz = []
     url_prefix_cz = "https://www.ikea.com/cz/cs/search/?q="
+    target_currency = "CZK"
     for product_id in product_id_list:
-        data_cz.append(scrape_product(driver, product_id, url_prefix_cz))
+        data_cz.append(scrape_product(driver, product_id, url_prefix_cz, target_currency))
+
 
     data_pl = []
     url_prefix_pl = "https://www.ikea.com/pl/pl/search/?q="
     for product_id in product_id_list:
-        data_pl.append(scrape_product(driver, product_id, url_prefix_pl))
+        data_pl.append(scrape_product(driver, product_id, url_prefix_pl, "PLN"))
 
     driver.quit()
 
+
     workbook = openpyxl.Workbook()
-    write_to_excel(workbook, data_cz, "Czech Data")
-    write_to_excel(workbook, data_pl, "Poland Data")
+    write_to_excel(workbook, data_cz, "Czech Data", "CZK")
+    write_to_excel(workbook, data_pl, "Poland Data", "PLN")
+    create_summary_sheet(workbook, data_cz, data_pl)
 
     workbook.save("ikea_products.xlsx")
 
